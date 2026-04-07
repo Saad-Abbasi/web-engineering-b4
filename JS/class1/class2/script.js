f2e17f1887fe690a288ea42de511272f305fda77
Saad Abbasi <sabbasi782@gmail.com>
2026-04-07 16:38:37 +0500

    JavaScript Started

---
+++ JS/class1/class2/script.js
@@ -0,0 +1,30 @@
+// if Else Statment in javaScript
+
+
+salary =80
+if(salary>100){
+    console.log(" i will join")
+}else{
+    console.log(" i will not join")
+
+}
+
+// Two Conditions
+salary =80
+creditCard = True
+if(salary>100 && creditCard){
+    console.log(" i will join")
+}else{
+    console.log(" i will not join")
+}
+
+
+
+salary =80
+creditCard = 'True'
+if(salary>100 && creditCard=='True'){
+    console.log(" i will join")
+}else{
+    console.log(" i will not join")
+}
+
---
+++ JS/class1/script.js
@@ -0,0 +1,12 @@
+num1 = 30;
+num2 = 40;
+
+sum = num1+num2;
+mul = num1*num2
+div = num1/num2
+sub = num1-num2
+
+console.log("Addition of ", num1, " and ",num2, " is : ", sum )
+console.log("Multiplicaiton of ", num1, " and ",num2, " is : ", mul )
+console.log("Division of ", num1, " and ",num2, " is : ", div )
+console.log("Subtraction of ", num1, " and ",num2, " is : ", sub )
